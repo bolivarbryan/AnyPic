bc73342c3764c755d1265f2bd624b9bb66336deb
Bryan <bolivarbryan@gmail.com>
2015-12-09 11:30:55 -0500

    first commit

---
+++ AnyPic/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  AnyPic
+//
+//  Created by Bryan A Bolivar M on 12/9/15.
+//  Copyright © 2015 Cookr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ AnyPic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AnyPic
+//
+//  Created by Bryan A Bolivar M on 12/9/15.
+//  Copyright © 2015 Cookr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
